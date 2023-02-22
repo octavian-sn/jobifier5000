8f55220d3a29dc04d3846ffb9f14c63bbd8b7a0c
Sulic Octavian <octavius1992@gmail.com>
2023-02-22 19:40:47 +0200

    Add testing applications and update applications method

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import List from './components/List';
+import uniqid from 'uniqid';
 
 function App() {
-  return <div></div>;
+  const [applications, setApplications] = useState(
+    JSON.parse(localStorage.getItem('applications')) || [
+      {
+        employer: 'BearingPoint',
+        title: 'Frontend Developer - Internship',
+        location: 'Sibiu',
+        salary: '-',
+        date: '22-Feb-2023',
+        id: uniqid(),
+      },
+      {
+        employer: 'BearingPoint',
+        title: 'Frontend Developer - Internship',
+        location: 'Sibiu',
+        salary: '-',
+        date: '22-Feb-2023',
+        id: uniqid(),
+      },
+    ]
+  );
+
+  const updateApplications = (newApp) => {
+    setApplications((prevApplications) => {
+      return [newApp, ...prevApplications];
+    });
+    // localStorage.setItem('applications', JSON.stringify(applications))
+  };
+
+  return (
+    <div>
+      <List applications={applications} />
+    </div>
+  );
 }
 
 export default App;
