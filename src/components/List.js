a0e59bc63daa267a569a7aad808a5147ff967601
Sulic Octavian <octavius1992@gmail.com>
2023-02-22 19:41:33 +0200

    Add List component for displaing applications

---
+++ src/components/List.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+function List({ applications }) {
+  const list = () =>
+    applications.map((application) => {
+      return (
+        <tr key={application.id}>
+          <td>{applications.indexOf(application)}</td>
+          <td>{application.employer}</td>
+          <td>{application.title}</td>
+          <td>{application.location}</td>
+          <td>{application.salary}</td>
+          <td>{application.date}</td>
+        </tr>
+      );
+    });
+
+  return (
+    <table>
+      <tr>
+        <th></th>
+        <th>Employer</th>
+        <th>Title</th>
+        <th>Location</th>
+        <th>Salary</th>
+        <th>Date</th>
+      </tr>
+      {list()}
+    </table>
+  );
+}
+
+export default List;
